529be9bec6cdf5f6ea9ae528f52d8c3de638fe3b
ashishnegi3003 <ashish.negi7117@gmail.com>
2026-03-05 15:12:24 +0530

    commit

--- src/main/java/SeleniumDemo.java
+++ src/main/java/SeleniumDemo.java
@@ -1,12 +1,16 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.sql.Driver;
 
 //First selenium project to visit a url and get its title
 public class SeleniumDemo {
     public static void main(String[] args) throws InterruptedException{
-        String browserTitle= chromeTest("https://www.amazon.in/");
-        System.out.println("User views :  " + browserTitle);
-
+        String chromeTitle= chromeTest("https://www.amazon.in/");
+        String firefoxTitle= firefoxTest("https://www.flipkart.in/");
+        System.out.println("On Chrome :  " + chromeTitle);
+        System.out.println("On Firefox :  " + firefoxTitle);
     }
     private static String chromeTest(String url) throws InterruptedException{
         WebDriver driver = new ChromeDriver();
@@ -15,7 +19,15 @@ public class SeleniumDemo {
         String title= driver.getTitle();
         driver.quit();
         return title;
-
-
     }
+    private static String firefoxTest(String url) throws InterruptedException{
+        System.setProperty("webdriverc.gekko.driver","src/main/resources/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get(url);
+        Thread.sleep(3000);
+        String title= driver.getTitle();
+        driver.quit();
+        return title;
+    }
+
 }
