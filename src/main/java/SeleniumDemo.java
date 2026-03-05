f5c8a4cdb8fce27a852e8136ead42c871225fa9a
ashishnegi3003 <ashish.negi7117@gmail.com>
2026-03-05 13:57:34 +0530

    commit

--- src/main/java/SeleniumDemo.java
+++ src/main/java/SeleniumDemo.java
@@ -1,14 +1,17 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+//First selenium project to visit a url and get its title
 public class SeleniumDemo {
-    public static void main(String[] args){
-        String title= GoBrowser("https://www.google.com");
-        System.out.println(title);
+    public static void main(String[] args) throws InterruptedException{
+        String browserTitle= chromeTest("https://www.amazon.in/");
+        System.out.println("User views :  " + browserTitle);
+
     }
-    private static String GoBrowser(String url){
+    private static String chromeTest(String url) throws InterruptedException{
         WebDriver driver = new ChromeDriver();
         driver.get(url);
+        Thread.sleep(3000);
         String title= driver.getTitle();
         driver.quit();
         return title;
